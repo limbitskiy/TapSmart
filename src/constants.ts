8abdb69f803d754584da4b9d65b35db12d957ac0
limbitskiy86 <gagarinbrood@gmail.com>
2024-09-19 11:02:16 +0300

    change botname

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export default {
   apiUrl: "https://api-dev.tapsmart.io",
   //   apiUrl: "https://api.tapsmart.io",
-  botName: "tapsmart_beta_bot",
-  appName: "tapsmart",
+  botName: "tiger8563_bot",
+  appName: "tigerapp",
 };
