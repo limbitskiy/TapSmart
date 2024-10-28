195d1860db6669df314cd45130691c43fb33ef11
limbitskiy86 <gagarinbrood@gmail.com>
2024-10-28 11:47:45 +0300

    debugging last screenshot#5

--- src/store/main.ts
+++ src/store/main.ts
@@ -351,13 +351,14 @@ export const useMainStore = defineStore("main", () => {
       leaderBoardImage.onload = () => {
         bgImage.onload = () => {
           const canvas = document.createElement("canvas");
-          const scale = window.devicePixelRatio || 1;
+          const scale = devicePixelRatio || 1;
 
           canvas.width = innerWidth * scale;
           canvas.height = innerHeight * scale;
           //   console.log(canvas);
           // console.log(fgImage.width);
           // console.log(fgImage.height);
+          debugMessages.value.push("scale:", scale);
           // debugMessages.value.push("canvas width:", canvas.width);
           // debugMessages.value.push("canvas height:", canvas.height);
           // debugMessages.value.push("bgimage width:", bgImage.width);
@@ -365,7 +366,7 @@ export const useMainStore = defineStore("main", () => {
           // debugMessages.value.push("leaderBoardImage width:", leaderBoardImage.width);
           // debugMessages.value.push("leaderBoardImage height:", leaderBoardImage.height);
           const ctx = canvas.getContext("2d");
-          ctx.scale(scale, scale);
+          // ctx.scale(scale, scale);
           ctx?.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
           ctx?.drawImage(leaderBoardImage, 32, canvas.height / 2 - 110);
 
