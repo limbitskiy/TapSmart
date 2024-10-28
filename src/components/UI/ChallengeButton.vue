280c4dad64da0f6f3b8ee1134ba62b6eea39e083
limbitskiy86 <gagarinbrood@gmail.com>
2024-10-28 18:00:17 +0300

    add attention seeker to challenge btn

--- src/components/UI/ChallengeButton.vue
+++ src/components/UI/ChallengeButton.vue
@@ -1,15 +1,24 @@
 <template>
-  <Button ref="challengeBtnRef" activeColor="#fcdcb0" class="flex-1 py-3 !px-2 relative" :class="{ 'bg-gray-300 text-gray-400': data.button_challenge?.disabled }" @click="onClick"
-    ><div class="flex justify-center gap-2">
-      <span v-bind="$attrs" class="text-base leading-4">{{ locale?.["button_challenge"] }}</span>
-      <Badge v-if="data?.questions_left > 0" class="relative bottom-1" :data="data?.questions_left" grey />
-    </div>
-  </Button>
+  <div class="challenge-btn-wrap relative flex-1 flex overflow-hidden rounded-xl" :class="{ heartbeat }">
+    <div ref="animationRef" class="lightray absolute -top-5 -left-[100%] w-[15px] h-[80px] bg-white z-10 rotate-45 opacity-80"></div>
+    <Button
+      ref="challengeBtnRef"
+      activeColor="#fcdcb0"
+      class="flex-1 py-3 !px-2 relative"
+      :class="{ 'bg-gray-300 text-gray-400': data.button_challenge?.disabled }"
+      @click="onClick"
+      ><div class="flex justify-center gap-2">
+        <span v-bind="$attrs" class="text-base leading-4">{{ locale?.["button_challenge"] }}</span>
+        <Badge v-if="data?.questions_left > 0" class="relative bottom-1" :data="data?.questions_left" grey />
+      </div>
+    </Button>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import { storeToRefs } from "pinia";
+import { useAnimate } from "@vueuse/core";
 
 // stores
 import { useMainStore } from "@/store/main";
@@ -25,6 +34,11 @@ const { data } = storeToRefs(store.battleStore);
 const { battles: locale } = storeToRefs(store.localeStore);
 
 const challengeBtnRef = ref();
+const animationRef = ref();
+
+const heartbeat = ref(false);
+
+let interval;
 
 const onClick = () => {
   if (data.value.button_challenge?.disabled) {
@@ -36,4 +50,21 @@ const onClick = () => {
     emit("challenge");
   }
 };
+
+const animateBtn = () => {
+  animationRef.value.animate({ left: "calc(150%)" }, 800);
+  setTimeout(() => {
+    heartbeat.value = true;
+  }, 1000);
+
+  setTimeout(() => {
+    heartbeat.value = false;
+  }, 500);
+};
+
+onMounted(() => {
+  interval = setInterval(() => {
+    animateBtn();
+  }, 15000);
+});
 </script>
