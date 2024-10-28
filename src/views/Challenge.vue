1fda32328017ca429561c576257b6cd92e0a3530
limbitskiy86 <gagarinbrood@gmail.com>
2024-10-28 22:15:15 +0300

    make screenshotInterval = 1/6

--- src/views/Challenge.vue
+++ src/views/Challenge.vue
@@ -113,7 +113,7 @@ onMounted(() => {
     startChallenge();
 
     challengeTimer.value = data.value?.battle_duration;
-    const screenshotInterval = data.value?.battle_duration / 8;
+    const screenshotInterval = data.value?.battle_duration / 6;
 
     challengeTimerInterval = setInterval(async () => {
       // make screenshot every (duration/screenshotInterval)
