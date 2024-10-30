9947da1ef0a9fbcc429aa46ee396f0ea9f21fd63
limbitskiy86 <gagarinbrood@gmail.com>
2024-10-30 23:08:14 +0300

    debugging share to story#5

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -282,7 +282,8 @@ const onFeedback = () => {
 };
 
 const postTestStory = () => {
-  tg.shareToStory("https://stories.tapsmart.io/193438653_26244560.mp4", {
+  // tg.shareToStory("https://stories.tapsmart.io/193438653_26244560.mp4", {
+  tg.shareToStory("https://stories-dev.tapsmart.io/123_456.mp4", {
     text: "TapSmart text",
     widget_link: { url: "https://t.me/TapSmartBot/TapSmartGame?startapp=fr193438653_sr1", name: "Widget link text" },
   });
