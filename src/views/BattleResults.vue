6dfdfdd641ffc6c6e5b78ad67f7c693a7ebed0b9
limbitskiy <gagarinbrood@gmail.com>
2024-10-30 15:24:58 +0300

    fixes#5

--- src/views/BattleResults.vue
+++ src/views/BattleResults.vue
@@ -1,57 +1,123 @@
 <template>
   <div class="results-cnt relative">
     <Transition name="fade">
-      <div v-if="generatingStory" class="story-generator-loader absolute inset-0 flex-1 grid h-dvh z-[9999] bg-[#222] place-items-center">
+      <div
+        v-if="generatingStory"
+        class="story-generator-loader absolute inset-0 flex-1 grid h-dvh z-[9999] bg-[#222] place-items-center"
+      >
         <div class="spinner max-w-[50vw] text-center flex flex-col gap-4">
           <div class="progressbar-wrap flex flex-col">
             <div class="percent-text text-[12px] text-gray-300">
               <span>{{ percent }}%</span>
             </div>
             <div class="progressbar h-[3px] w-full bg-gray-400 rounded-full">
-              <div class="value h-[3px] rounded-full bg-[var(--accent-color)]" :style="{ width: percent + '%' }"></div>
+              <div
+                class="value h-[3px] rounded-full bg-[var(--accent-color)]"
+                :style="{ width: percent + '%' }"
+              ></div>
             </div>
           </div>
-          <span class="text-lg z-50">{{ locale?.["tg_story_loader"] ?? "Создаем историю вашего успеха..." }}</span>
+          <span class="text-lg z-50">{{
+            locale?.["tg_story_loader"] ?? "Создаем историю вашего успеха..."
+          }}</span>
         </div>
       </div>
     </Transition>
 
     <!-- <div v-for="item in HTMLSnapshots" ref="htmlEl" class="screenshot-stage flex-1 flex flex-col h-dvh bg-transparent" v-html="item"></div> -->
-    <div v-if="generatingStory" ref="htmlEl" class="screenshot-stage flex-1 flex flex-col h-dvh bg-transparent" v-html="html"></div>
+    <div
+      v-if="generatingStory"
+      ref="htmlEl"
+      class="screenshot-stage flex-1 flex flex-col h-dvh bg-transparent"
+      v-html="html"
+    ></div>
 
     <div id="battle-results" class="flex-1 flex flex-col p-4 h-dvh">
       <BackgroundImage type="purple" />
       <Profile />
 
-      <BackgroundPill class="py-8 mt-4 overflow-y-hidden flex-1 z-10">
-        <div ref="leaderboardRef" class="final-screenshot bg-[var(--grey-dark)] w-full">
-          <div class="pill-header flex items-center justify-between">
-            <span class="bg-pill-title">{{ locale?.["battle_results_title"] || "Battle results:" }}</span>
-          </div>
+      <BackgroundPill class="py-8 mt-4 flex-1 z-10 overflow-hidden">
+        <div class="scrollable-cnt flex-1 flex flex-col gap-8 overflow-y-auto">
+          <!-- leaderboard -->
+          <div
+            ref="leaderboardRef"
+            class="final-screenshot bg-[var(--grey-dark)] w-full"
+          >
+            <div class="pill-header flex items-center justify-between">
+              <span class="bg-pill-title">{{
+                locale?.["battle_results_title"] || "Battle results:"
+              }}</span>
+            </div>
 
-          <div class="scrollable-cnt flex-1 overflow-y-auto mt-2">
-            <div class="leaderboard flex flex-col gap-1 pt-4">
-              <Pill v-for="player in leaderboardSorted" :key="player.id" class="py-2 flex items-center justify-between gap-4" color="light">
-                <div class="player-meta leading-3 flex gap-4 items-center">
-                  <div class="rounded-full bg-[var(--grey-dark)] w-[30px] h-[30px] grid place-items-center">
-                    <span class="league exo-black text-lg mb-[1px]" :style="`color: ${getPlayerColor(player)}`">{{ player.position }}</span>
+            <div class="flex-1 mt-2">
+              <div
+                v-if="generatingStory"
+                class="leaderboard-capped flex flex-col gap-1 pt-4"
+              >
+                <Pill
+                  v-for="player in leaderboardSortedCapped"
+                  :key="player.id"
+                  class="py-2 flex items-center justify-between gap-4"
+                  color="light"
+                >
+                  <div class="player-meta leading-3 flex gap-4 items-center">
+                    <div
+                      class="rounded-full bg-[var(--grey-dark)] w-[30px] h-[30px] grid place-items-center"
+                    >
+                      <span
+                        class="league exo-black text-lg mb-[1px]"
+                        :style="`color: ${getPlayerColor(player)}`"
+                        >{{ player.position }}</span
+                      >
+                    </div>
+                    <span class="fira-bold text-lg max-w-1/2 text-ellipsis">{{
+                      player.name
+                    }}</span>
                   </div>
-                  <span class="fira-bold text-lg max-w-1/2 text-ellipsis">{{ player.name }}</span>
-                </div>
-                <span class="bolts exo-black text-[var(--accent-color)]">{{ player?.score || 0 }}</span>
-              </Pill>
+                  <span class="bolts exo-black text-[var(--accent-color)]">{{
+                    player?.score || 0
+                  }}</span>
+                </Pill>
+              </div>
+
+              <div v-else class="leaderboard flex flex-col gap-1 pt-4">
+                <Pill
+                  v-for="player in leaderboardSorted"
+                  :key="player.id"
+                  class="py-2 flex items-center justify-between gap-4"
+                  color="light"
+                >
+                  <div class="player-meta leading-3 flex gap-4 items-center">
+                    <div
+                      class="rounded-full bg-[var(--grey-dark)] w-[30px] h-[30px] grid place-items-center"
+                    >
+                      <span
+                        class="league exo-black text-lg mb-[1px]"
+                        :style="`color: ${getPlayerColor(player)}`"
+                        >{{ player.position }}</span
+                      >
+                    </div>
+                    <span class="fira-bold text-lg max-w-1/2 text-ellipsis">{{
+                      player.name
+                    }}</span>
+                  </div>
+                  <span class="bolts exo-black text-[var(--accent-color)]">{{
+                    player?.score || 0
+                  }}</span>
+                </Pill>
+              </div>
             </div>
           </div>
-        </div>
 
-        <!-- ad -->
-        <div class="ad flex flex-col flex-1 items-center justify-center mt-8">
-          <Ad
-            :image="data?.['battle_results_ad_image']"
-            :title="locale?.['battle_results_ad_title']"
-            :text="locale?.['battle_results_ad_text']"
-            :tooltip="locale?.['tooltip_battle_results_ad']"
-          />
+          <!-- ad -->
+          <div class="ad flex flex-col flex-1 items-center justify-center">
+            <Ad
+              :image="data?.['battle_results_ad_image']"
+              :title="locale?.['battle_results_ad_title']"
+              :text="locale?.['battle_results_ad_text']"
+              :tooltip="locale?.['tooltip_battle_results_ad']"
+            />
+          </div>
         </div>
 
         <!-- buttons -->
@@ -131,6 +197,14 @@ const leaderboardSorted = computed(() => {
   return clone.sort((a, b) => b.score - a.score);
 });
 
+const leaderboardSortedCapped = computed(() => {
+  if (!data.value.battle_results_leaderboard?.length) return [];
+
+  const clone = [...data.value.battle_results_leaderboard];
+
+  return clone.sort((a, b) => b.score - a.score).slice(0, 4);
+});
+
 const getPlayerColor = (player: {}) => {
   if (player.isPlayer) {
     return "#26df26";
@@ -169,7 +243,9 @@ onMounted(async () => {
     }, 100);
 
     console.log(`creating final image`);
-    const url = await htmlToImage.toJpeg(leaderboardRef.value, { quality: 0.85 });
+    const url = await htmlToImage.toJpeg(leaderboardRef.value, {
+      quality: 0.85,
+    });
 
     console.log(`final image created`);
 
