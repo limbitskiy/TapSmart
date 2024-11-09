03e0bb5abe4086f3e97311693b9bd38c6ef07f02
limbitskiy86 <gagarinbrood@gmail.com>
2024-11-09 14:20:19 +0300

    finishing leaderboard#10

--- src/views/Leaders.vue
+++ src/views/Leaders.vue
@@ -1,10 +1,10 @@
 <template>
   <div ref="screenshotEl" id="leaders" class="flex flex-col gap-4 flex-1 p-4 relative">
     <!-- debug messages -->
-    <div class="fixed top-0 left-0 right-0 bg-black text-white flex flex-col text-sm z-50">
+    <!-- <div class="fixed top-0 left-0 right-0 bg-black text-white flex flex-col text-sm z-50">
       <span>rect top: {{ debugMsg.recttop }}</span>
       <span>inner height: {{ debugMsg.innerheight }}</span>
-    </div>
+    </div> -->
 
     <div class="top-part">
       <div class="icon-and-title flex items-center gap-2">
@@ -169,8 +169,6 @@
       </BackgroundPill>
     </div>
 
-    <!-- <div class="debug fixed top-0">visible: {{ targetIsVisible }}</div> -->
-
     <!-- bottom player pill -->
     <Transition name="slight-move-up">
       <div v-if="!targetIsVisible" class="fixed-player fixed left-8 right-8" :class="[leaders?.['has_story'] ? 'bottom-[135px]' : 'bottom-[80px]']">
@@ -264,16 +262,15 @@ const filter = ref<Filter>({
   friends: false,
   bigFilter: "bolt",
 });
-const target = ref();
 const targetIsVisible = ref(true);
 const leaderListLoading = ref(false);
 const isTooltipModal = ref(false);
 const screenshotEl = ref();
 
-const debugMsg = ref({
-  recttop: 0,
-  innerheight: 0,
-});
+// const debugMsg = ref({
+//   recttop: 0,
+//   innerheight: 0,
+// });
 
 const computedPlayer = computed(() => leaders.value?.data?.find((player) => player.isPlayer));
 
@@ -331,14 +328,9 @@ const onCloseSubtitleModal = () => {
 
 const checkPlayerVisibility = () => {
   const playerPill = document.querySelector("#player-pill");
-  console.log(playerPill);
 
   if (playerPill) {
     const rect = playerPill.getBoundingClientRect();
-    console.log(rect);
-
-    debugMsg.value.recttop = rect.top;
-    debugMsg.value.innerheight = innerHeight;
 
     if (rect.top < innerHeight - 70 && rect.top > 0) {
       targetIsVisible.value = true;
